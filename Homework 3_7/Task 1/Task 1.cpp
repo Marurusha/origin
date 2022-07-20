6a8a926f5a0387bb242c36b1132e7ae56a9eeb0d
Марина Герасимова <marurusha@mail.ru>
2022-07-21 00:40:35 +0300

    Task 1 from lesson 2 uploaded

---
+++ Homework 3_7/Task 1/Task 1.cpp
@@ -0,0 +1,32 @@
+﻿#define MODE 1
+#ifndef MODE
+#error Can't find the nessesery const
+#endif
+#include<iostream>
+
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	if (MODE == 0) {
+		std::cout << "Работаю в режиме тренировки" << std::endl;
+	}
+	else if (MODE == 1) {
+		std::cout << "Работаю в боевом режиме" << std::endl;
+		int a, b;
+		std::cout << "Введите первое число: ";
+		std::cin >> a;
+		std::cout << "Введите второе число: ";
+		std::cin >> b;
+		std::cout << "Результат сложения: " << add(a, b) << std::endl;
+	}
+	else {
+		std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+		return 1;
+	}
+	system("pause");
+	return 0;
+}
+
