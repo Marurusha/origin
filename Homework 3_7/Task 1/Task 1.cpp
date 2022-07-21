02fd02fe22258ad1dd6eeb0ba9f4aadd6016f295
Марина Герасимова <marurusha@mail.ru>
2022-07-21 20:05:00 +0300

    Condition for function definition corrected

--- Homework 3_7/Task 1/Task 1.cpp
+++ Homework 3_7/Task 1/Task 1.cpp
@@ -1,18 +1,22 @@
-﻿#define MODE 1
+﻿#define MODE 0
 #ifndef MODE
 #error Can't find the nessesery const
 #endif
 #include<iostream>
 
+#if MODE 1
 int add(int num1, int num2) {
 	return num1 + num2;
 }
+#endif
+
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	if (MODE == 0) {
 		std::cout << "Работаю в режиме тренировки" << std::endl;
 	}
+#if MODE 1
 	else if (MODE == 1) {
 		std::cout << "Работаю в боевом режиме" << std::endl;
 		int a, b;
@@ -22,6 +26,7 @@ int main()
 		std::cin >> b;
 		std::cout << "Результат сложения: " << add(a, b) << std::endl;
 	}
+#endif
 	else {
 		std::cout << "Неизвестный режим. Завершение работы" << std::endl;
 		return 1;
