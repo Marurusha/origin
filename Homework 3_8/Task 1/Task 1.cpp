53d48fcdda10f1753ba311643a112c11c9e6f6b7
Марина Герасимова <marurusha@mail.ru>
2022-08-07 20:17:41 +0300

    Corrected type of base ex, arguments for ex

--- Homework 3_8/Task 1/Task 1.cpp
+++ Homework 3_8/Task 1/Task 1.cpp
@@ -1,20 +1,35 @@
 ﻿#include <iostream>
 #include <string>
 
-class bad_length : public std::exception {
+class bad_length : public std::runtime_error {
 public:
-	const char* what() const override { return "Вы ввели слово запретной длины!"; }
+	bad_length(const char* msg, std::string word, int length) : runtime_error(msg) {
+		fword = word;
+		this -> length = length;
+	}
+	//В текущей реализации гет-методы не используются, добавила на перспективу
+	std::string get_fword() {
+		return fword;
+	}
+	int get_length() {
+		return length;
+	}
+private:
+	std::string fword;
+	int length;
 };
 
 int function(std::string str, int forbidden_length) {
-	if (str.length() == forbidden_length) throw bad_length();
+	if (str.length() == forbidden_length) {
+		throw bad_length("Введено слово запрещенной длины!", str, str.length());
+	}
 	return str.length();
 }
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int stop;
+	int stop, length;
 	std::string word;
 	try {
 		do {
@@ -22,11 +37,13 @@ int main()
 			std::cin >> stop;
 			std::cout << "Введите слово: ";
 			std::cin >> word;
-			std::cout << "Длина слова " << word << " равна " << function(word, stop) << std::endl;
+			length = function(word, stop);
+			std::cout << "Длина слова '" << word << "' равна " << length << std::endl;
 		} while (true);
 	}
-	catch (const bad_length& stop_programm) {
-		std::cout << stop_programm.what() << " До свидания" << std::endl;
+	catch (bad_length& stop_programm) {
+		std::cout << stop_programm.what() << std::endl << "До свидания" << std::endl;
+		system("pause");
 		return 1;
 	}
 
