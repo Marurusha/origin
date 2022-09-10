7df4f81d84cafb6221f32660f12312cff529870f
Марина Герасимова <marurusha@mail.ru>
2022-09-10 13:50:12 +0300

    Task 1v1 from lesson 11 uploaded

---
+++ Homework 3_11/Task1_Cmake/Greeter.cpp
@@ -0,0 +1,11 @@
+﻿#include "Greeter.h"
+
+Greeter::Greeter(std::string username) {
+	name = username;
+}
+
+std::string Greeter::greet() {
+	std::string hello;
+	hello = "Здравствуйте, " + name + "!\n";
+	return hello;
+}
---
+++ Homework 3_11/Task1_Cmake/Greeter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <iostream>
+#include <Windows.h>
+
+class Greeter {
+private:
+	std::string name;
+
+public:
+	Greeter(std::string username);
+	std::string greet();
+};
