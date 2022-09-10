bc4a9098da59094bc14dbb38182753918e83b4ab
Марина Герасимова <marurusha@mail.ru>
2022-09-10 17:43:39 +0300

    Encode corrected

---
+++ Homework 3_11/Task1/Task1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Greeter.h"
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string username;
+    std::cout << "Введите имя: ";
+    std::cin >> username;
+
+    Greeter greeter(username);
+    std::cout << greeter.greet();
+
+    return 0;
+}
